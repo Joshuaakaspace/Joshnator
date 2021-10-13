f1571c7f83794f0924966731cf364539a2ea76c6
Joshuaakaspace <joshuanishanth120@gmail.com>
2021-10-13 10:14:02 +0530

    preprocess

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md', 'r') as file:
+	long_description = file.read()
+
+
+setuptools.setup(
+	name = 'joshnator', #this should be unique
+	version = '0.0.1',
+	author = 'Joshua Nishanth',
+	author_email = 'joshuanishanth120@gmail.com',
+	description = 'data cleaning package',
+	long_description = long_description,
+	long_description_content_type = 'text/markdown',
+	packages = setuptools.find_packages(),
+	classifiers = [
+	'Programming Language :: Python :: 3',
+	'License :: OSI Aproved :: MIT License',
+	"Operating System :: OS Independent"],
+	python_requires = '>=3.5'
+	)
