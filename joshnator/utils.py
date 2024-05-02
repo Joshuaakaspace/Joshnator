e737166c8605378f084275ba729026bd44a3fcfc
Joshua <75612919+Joshuaakaspace@users.noreply.github.com>
2024-05-03 00:31:31 +0530

    Update utils.py

--- joshnator/utils.py
+++ joshnator/utils.py
@@ -165,9 +165,9 @@ def _get_ngram(df, col, ngram_range):
 	ngram = sorted(ngram.items(), key = lambda x: x[1], reverse=True)
 
 	return ngram
-def _clean_sentence_with_stop_words(title):
-    title = ''.join([char for char in title if char not in string.punctuation])
-    title = ''.join([char for char in title if not char.isdigit()])
+def _get_sentence_stop(x):
+    x = ''.join([char for char in x if char not in string.punctuation])
+    x = ''.join([char for char in x if not char.isdigit()])
     stop_words = set(stopwords.words('english'))
-    title = ' '.join([word for word in title.split() if word.lower() not in stop_words])
-    return title.strip()
+    x = ' '.join([word for word in x.split() if word.lower() not in stop_words])
+    return x.strip()
