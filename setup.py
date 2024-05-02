5e61b4d3cee9adfa48f154ec4e48c4432f68eee4
Joshua <75612919+Joshuaakaspace@users.noreply.github.com>
2024-05-03 00:04:02 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
 import setuptools
 
 with open('README.md', 'r') as file:
-	long_description = file.read()
-
+    long_description = file.read()
 
 setuptools.setup(
-	name = 'joshnator',
-	version = '0.0.4.',
-	author = 'Joshua Nishanth',
-	author_email = 'joshuanishanth120@gmail.com',
-	description = 'data cleaning package',
-	long_description = long_description,
-	long_description_content_type = 'text/markdown',
-	packages = setuptools.find_packages(),
-	classifiers = [
-	'Programming Language :: Python :: 3',
-	'License :: OSI Aproved :: MIT License',
-	"Operating System :: OS Independent"],
-	python_requires = '>=3.5'
-	)
+    name='joshnator',
+    version='0.0.4',
+    author='Joshua Nishanth',
+    author_email='joshuanishanth120@gmail.com',
+    description='Data cleaning package',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Joshuaakaspace/joshnator',  # Add your repository URL
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3.5',
+    install_requires=[
+        'pandas>=1.0',
+        'numpy>=1.15',
+    ]
+)
+
